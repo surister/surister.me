8f0ac99bf807127cb1df2c8d965982107ce75ac9
surister <surister98@gmail.com>
2022-01-31 21:51:51 +0100

    Add navbar component, css and FA packages

--- src/main.js
+++ src/main.js
@@ -3,4 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import Oruga from '@oruga-ui/oruga-next'
+
+import { bulmaConfig } from '@oruga-ui/theme-bulma'
+import '../sass/style.scss'
+
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faUser, faGithub);
+dom.watch();
+
+
+createApp(App).use(store).use(router).use(Oruga, bulmaConfig).mount('#app')
